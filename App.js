734ed8e54c71b09ba869236594a59e63ea1834ab
unknown <vanphuvnpt@gmail.com>
2022-03-17 22:45:06 +0700

    to

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <AppNavigator/>
+    </Provider>
   );
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+1. Install depennence
+- yarn add @react-navigation/native
+- @react-navigation/native-stack
+-react-native-safe-area-context
+-react-native-screens
+-expo install react-redux redux redux-devtools-extension redux-thunk
+expo install @react-native-async-storage/async-storage
